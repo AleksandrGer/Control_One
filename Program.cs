a2784fe6cf59f92e057a24596bca62d43c0e602e
Aleksandr G <gerbl444@gmail.com>
2024-03-05 20:14:54 +0300

    Добавили решение задачи в виде програмного кода

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] mass1 = ["Hgggo", "2", "ld", ":-)"];
+int size_mass2 = 0;
+
+for (int i = 0; i < mass1.Length; i++)
+{
+    if (mass1[i].Length <= 3)
+    {
+        size_mass2 = size_mass2 += 1;
+    }
+}
+
+string[] mass2 = new string[size_mass2];
+int j = 0;
+
+for (int i = 0; i < mass1.Length; i++)
+{
+    if (mass1[i].Length <= 3)
+    {
+        mass2[j] = mass1[i];
+        j += 1;
+    }
+}
+
+Console.WriteLine($"Массив: [ {string.Join("; ", mass2)} ]");
